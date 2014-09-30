5aa7d66f6922850ca411c11987c6c94e11518460
Wei Cheng <calvinpohwc@gmail.com>
2014-10-01 01:06:01 +0800

    Added FileHandler to Read and Write from/to files.

---
+++ src/main/java/com/the/todo/io/FileHandler.java
@@ -0,0 +1,65 @@
+/**
+ * This file is part of TheTODO, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) 2014 TheTODO
+ * Copyright (c) Poh Wei Cheng <calvinpohwc@gmail.com>
+ *				 Chen Kai Hsiang <kaihsiang95@gmail.com>
+ *				 Khin Wathan Aye <y.caiyun@gmail.com>
+ *				 Neo Eng Tai <neoengtai@gamil.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.the.todo.io;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class FileHandler {
+
+	final static String FILE_NAME = "todo.json";
+	final static Charset ENCODING = StandardCharsets.UTF_8;
+
+	public static String readFile(String fileName) throws IOException {
+		Path path = Paths.get(fileName);
+		StringBuilder stringBuilder = new StringBuilder();
+
+		try (Scanner scanner = new Scanner(path, ENCODING.name())) {
+			while (scanner.hasNextLine()) {
+				stringBuilder.append(scanner.nextLine());
+			}
+		}
+
+		return stringBuilder.toString();
+	}
+
+	public static void writeFile(String fileName, String contents)
+			throws IOException {
+		Path path = Paths.get(fileName);
+		try (BufferedWriter writer = Files.newBufferedWriter(path, ENCODING)) {
+			writer.write(contents);
+		}
+	}
+}
