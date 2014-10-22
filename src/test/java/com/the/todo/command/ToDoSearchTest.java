1561076f0369e391834edbb71160234078a4274b
neoengtai <neoengtai@gmail.com>
2014-10-23 00:51:05 +0800

    Implement search by category and keywords

--- src/test/java/com/the/todo/command/ToDoSearchTest.java
+++ src/test/java/com/the/todo/command/ToDoSearchTest.java
@@ -20,26 +20,46 @@ public class ToDoSearchTest {
 	private ToDo todo1 = new ToDo("Lorem");
 	private ToDo todo2 = new ToDo("ipsum dolor sit");
 	private ToDo todo3 = new ToDo("dolor sit amet");
+	private ToDo todo4 = new ToDo("amet consectetur adipiscing elit.");
 
 	ArrayList<ToDo> updateListStub;
 	InMemoryStore storeStub;
 
 	@Before
 	public void setUp() {
+		todo1.setCategory("+search");
+		todo2.setCategory("+add");
+		todo3.setCategory("+test");
+		todo4.setCategory("+test");
+
 		storeStub = new InMemoryStore();
 		storeStub.save(todo1);
 		storeStub.save(todo2);
 		storeStub.save(todo3);
+		storeStub.save(todo4);
 
 		updateListStub = new ArrayList<ToDo>();
 	}
 
 	@Test
 	public void test() {
+		searchTest("");
 		searchTest("or");
-		searchTest("Lorem", "Lorem");
-		searchTest("amet", "dolor sit amet");
+		searchTest("Lorem ", "Lorem");
+		searchTest("amet", "dolor sit amet", "amet consectetur adipiscing elit.");
+		searchTest("elit.", "amet consectetur adipiscing elit.");
 		searchTest("dolor sit", "ipsum dolor sit", "dolor sit amet");
+
+		searchTest("+");
+		searchTest("+search", "Lorem");
+		searchTest("+test", "dolor sit amet",
+				"amet consectetur adipiscing elit.");
+
+		searchTest("or +search");
+		searchTest("dolor sit +add", "ipsum dolor sit");
+		searchTest("amet +test", "dolor sit amet",
+				"amet consectetur adipiscing elit.");
+
 	}
 
 	public void searchTest(String query, String... expected) {
